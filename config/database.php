597ae8ce473b48df2ae30926b46bf06e2781d12f
Bulat <b.kamalov@php73.ru>
2020-05-13 20:09:29 +0400

    gitlab ci [ci buildNginx]

--- config/database.php
+++ config/database.php
@@ -78,6 +78,20 @@ return [
             'sslmode' => 'prefer',
         ],
 
+        'test' => [
+            'driver' => 'pgsql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge') . '_test',
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'schema' => 'public',
+            'sslmode' => 'prefer',
+        ],
+
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DATABASE_URL'),
