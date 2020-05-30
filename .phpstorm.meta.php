ec35d4ccb20b10527e063cd60fbe74620ba43017
Bulat <b.kamalov@php73.ru>
2020-05-30 21:19:33 +0400

    create default auth, node in docker, configure webpack.mix.js for generating js and css

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-05-30 15:38:02.
+    * Generated on 2020-05-30 16:10:52.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
